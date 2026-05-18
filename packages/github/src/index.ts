070c85a96771ad914f44bff262d9128db52e94c7
Jane-o-O-o-O <Jane-o-O-o-O@users.noreply.github.com>
2026-05-18 13:26:02 +0000

    refactor: improve code organization in index

--- packages/github/src/index.ts
+++ packages/github/src/index.ts
@@ -114,3 +114,21 @@ export class AuthmiddlewareHandler {
     return { status: "processed", data, handler: this.constructor.name };
   }
 }
+
+// [2026-05-18] Refactor: simplified index
+abstract class BaseHandler<TOptions extends Record<string, unknown> = {}> {
+  protected options: TOptions;
+  protected logger: Console;
+
+  constructor(options: Partial<TOptions> = {}) {
+    this.options = { ...this.defaults(), ...options } as TOptions;
+    this.logger = console;
+  }
+
+  protected abstract defaults(): TOptions;
+  abstract process(data: unknown): Promise<unknown>;
+
+  protected handleError(err: Error): void {
+    this.logger.error(`[${this.constructor.name}] ${err.message}`);
+  }
+}
